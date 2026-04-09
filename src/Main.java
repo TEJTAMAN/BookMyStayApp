0171b955001b66d9b5b240d8ca62b634315de0d8
TEJTAMAN <rtejtaman@gmail.com>
2026-04-09 13:54:57 +0530

    UC1: add welcome message module

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Welcome to the Hotel Booking Management System");
-        System.out.println("System initialized successfully.");
+        System.out.println("Welcome to the Hotel Booking Management System...");
+        System.out.println("System initialized successfully...");
     }
 }
